6146ee474a7ddb71378d226076f7b3553a63432d
park-yeongjun <pyj9293@gmail.com>
2022-10-17 04:58:11 +0900

    ocr 기능 추가

---
+++ server/functions/index.js
@@ -0,0 +1,29 @@
+const functions = require("firebase-functions");
+const vision = require('@google-cloud/vision');
+const app = functions.region('asia-northeast3');
+
+
+exports.getOcrText = app.https.onRequest(async (request, response) => {
+  response.send(await quickstart(request.body.base64Image));
+});
+
+async function quickstart(arrByte) {
+  const regex = /[^0-9]/g;  // 숫자 체크
+  const client = new vision.ImageAnnotatorClient({ keyFilename: 'three-youth-app-vision-key.json' });
+  const [textDetection] = await client.textDetection({
+    image: { content: arrByte }
+  });
+  const textAnnotations = textDetection.textAnnotations;
+
+  console.log('Text:');
+  var results = [];
+  textAnnotations.forEach(text => {
+    var tmp = text.description.replace(regex, '');
+    if (tmp.length != 0 && !tmp.includes('651')) {
+      results.push(tmp);
+    }
+  });
+
+  return results;
+}
+
