0483ba77e589e6921f98cd2dbd19fe456a3c930d
park-yeongjun <pyj9293@gmail.com>
2023-02-27 20:02:44 +0900

    내정보 키 몸무게 제한

--- lib/screens/profile_setting/components/profile_setting_height_input_form.dart
+++ lib/screens/profile_setting/components/profile_setting_height_input_form.dart
@@ -1,10 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:three_youth_app/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 
-class ProfileSettingHeightInputForm extends StatelessWidget {
-  const ProfileSettingHeightInputForm({Key? key}) : super(key: key);
+import 'package:three_youth_app/providers/user_provider.dart';
+
+import '../../../utils/toast.dart';
+
+class ProfileSettingHeightInputForm extends StatefulWidget {
+  final double height;
+
+  const ProfileSettingHeightInputForm({
+    Key? key,
+    required this.height,
+  }) : super(key: key);
+
+  @override
+  State<ProfileSettingHeightInputForm> createState() =>
+      _ProfileSettingHeightInputFormState();
+}
+
+class _ProfileSettingHeightInputFormState
+    extends State<ProfileSettingHeightInputForm> {
+  final TextEditingController _heightCon = TextEditingController();
+
+  @override
+  void initState() {
+    _heightCon.text = widget.height.toString();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,34 +44,46 @@ class ProfileSettingHeightInputForm extends StatelessWidget {
           width: _screenWidth - 100.0,
           height: 40.0,
           child: TextFormField(
-            initialValue: '$_height',
-            enableInteractiveSelection: false,
-            keyboardType: const TextInputType.numberWithOptions(decimal: true),
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp(r'^\d{0,4}\.?\d{0,1}')),
-            ],
-            textAlign: TextAlign.center,
-            style: const TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: _screenHeight * 0.015),
-              hintText: 'cm',
-              hintStyle: const TextStyle(color: Colors.white),
-              // ignore: use_full_hex_values_for_flutter_colors
-              fillColor: const Color(0xff00000033).withOpacity(0.25),
-              filled: true,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(40.0),
-                borderSide: const BorderSide(color: Colors.white, width: 1.5),
+              controller: _heightCon,
+              enableInteractiveSelection: false,
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(
+                    RegExp(r'^\d{0,4}\.?\d{0,1}')),
+              ],
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: _screenHeight * 0.015),
+                hintText: 'cm',
+                hintStyle: const TextStyle(color: Colors.white),
+                // ignore: use_full_hex_values_for_flutter_colors
+                fillColor: const Color(0xff00000033).withOpacity(0.25),
+                filled: true,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                  borderSide: const BorderSide(color: Colors.white, width: 1.5),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                  borderSide: const BorderSide(color: Colors.white),
+                ),
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(40.0),
-                borderSide: const BorderSide(color: Colors.white),
-              ),
-            ),
-            onChanged: (value) =>
-                context.read<UserProvider>().onChangeHeight(value: value),
-          ),
+              onChanged: (value) {
+                if (value.isNotEmpty) {
+                  if (double.parse(value) <= 300) {
+                    context.read<UserProvider>().onChangeHeight(value: value);
+                  } else {
+                    context
+                        .read<UserProvider>()
+                        .onChangeHeight(value: widget.height.toString());
+                    _heightCon.clear();
+                    showToast('300cm 이하로 입력해주세요');
+                  }
+                }
+              }),
         ),
         const Text(
           'cm',
